c0e16ccd1ae3d7e3dd8b6ba6e4308b49e07527c8
Amir Vosough <amir.vosough@tba.group>
2019-11-13 15:53:50 +0100

    Initial commit

---
+++ src/main/java/ir/amv/snippets/buildjenk/BuildJenkApplication.java
@@ -0,0 +1,13 @@
+package ir.amv.snippets.buildjenk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildJenkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildJenkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/amv/snippets/buildjenk/BuildJenkApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.amv.snippets.buildjenk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildJenkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
